2479936c920ab2664a831d02a83c73ddc6d85790
Roman Podobny <tazer16179@gmail.com>
2021-10-26 21:32:38 +0300

    new commit slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import HeadModal from './Components/ModalComponents/HeadModal';
+import SliderComponent from './Components/Slider/SliderComponent';
 function App() {
   return (
     <div className="App">
       <HeadModal/>
+      <SliderComponent/>
     </div>
   );
 }
---
+++ src/Components/Slider/SliderComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import slider from "./SliderComponent.module.css"
+import Carousel from "react-bootstrap/Carousel"
+import code from "./../../assets/code.jpg"
+import ship from "./../../assets/ship.jpg"
+const SliderComponent = () => {
+    return (
+<Carousel>
+    <Carousel.Item>
+        <img 
+        src={ship} alt="ship" className="d-block w-100"/>
+        <Carousel.Caption>
+            <h3>WhatterFool </h3>
+            <p className=""></p>
+        </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+        <img 
+        src={code} alt="code" className="d-block w-100"/>
+        <Carousel.Caption>
+            <h3>WhatterFool </h3>
+            <p className=""></p>
+        </Carousel.Caption>
+    </Carousel.Item>
+</Carousel>
+    )
+}
+
+export default SliderComponent
